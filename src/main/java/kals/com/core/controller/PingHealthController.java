01efbc3d5feace9a8e5e1ad513a191394d652451
Midhun Elangovan <midhunabimanyue@gmail.com>
2026-07-16 08:49:07 +0530

    Removed regex which is not needed for generic ping controller, end point takes application's context path defaultly.

--- src/main/java/kals/com/core/controller/PingHealthController.java
+++ src/main/java/kals/com/core/controller/PingHealthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/{*context-path}/ping")
+@RequestMapping("/ping")
 public class PingHealthController {
 
     @Value("${spring.application.name:}")
